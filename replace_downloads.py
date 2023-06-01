4d2db118d1477c15c2034abaa2eab0b343fcaba6
Logan Thomas <logannormanthomas@protonmail.com>
2023-06-01 00:42:40 -0600

    filtered down to http request/response and even further into .exe files

--- replace_downloads.py
+++ replace_downloads.py
@@ -4,8 +4,15 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.DNSRR):
-
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("HTTP Response")
+            print(scapy_packet.show())
 
     packet.accept()
 
